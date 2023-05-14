2536221c079399a199ef70ad221a508ffeeb5349
Alnitakr51 <gadieljj34@gmail.com>
2023-05-14 02:18:38 +0200

    Aprendizaje probabilistico

---
+++ 2023_05_13_0_10_02_agrupamiento_no_supervisado.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 14 00:32:44 2023
+
+@author: Gadiel Jimenez
+"""
+
+from sklearn.cluster import AgglomerativeClustering
+import numpy as np
+
+# Generamos un conjunto de datos aleatorios para agrupar
+X = np.random.rand(100, 2)
+print(X)
+# Creamos un objeto AgglomerativeClustering con 3 clústeres
+agg_clustering = AgglomerativeClustering(n_clusters=3)
+
+# Ajustamos el modelo a los datos
+agg_clustering.fit(X)
+
+# Obtenemos las etiquetas de clústeres para cada punto de datos
+labels = agg_clustering.labels_
+
+# Imprimimos los resultados
+
+print("Etiquetas de clústeres: ", labels)
